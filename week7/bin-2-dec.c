20eb1071ef515f3c36bde634192ef3d98f5ddfab
Frogger <66127998+frog-lake@users.noreply.github.com>
2020-11-20 19:28:22 +0000

    Update bin-2-dec.c

--- week7/bin-2-dec.c
+++ week7/bin-2-dec.c
@@ -1,109 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <string.h>
-#include <ctype.h>
 
-// a program to take in numbers from the command line
-// and output how many evens, odds and primes there are
+void convert2bin(int *binary);
 
-int count_numbers(int args[], int argc);
-int check_prime(int num);
-
+int result;
 
 int main(int argc, char *argv[])
 {
-    
-    int i, numbers[argc];
-    
-    if(argc < 2){
-        printf("insufficient command line arguments.\n");
+    char c[1000];
+    int i;
+    // puts argv[1] into c
+    sprintf(c, "%s", argv[1]);
+
+    // puts args into 1 string
+    if(argc > 2) {
+        for(i = 2; i < argc; i++){
+            strcat(c, argv[i]);
+        }
+    }
+
+    // string length
+    int len = strlen(c);
+
+    // tests how many digits there are (max 8)
+    // returns 0 if over character limit
+    if(len > 8){
+        printf("Too many binary digits entered.\n");
         return 0;
     }
-    
-    // for checking if there are letters in argv
-    char c[1000];
-    sprintf(c, "%s", argv[1]);
-    for(i = 2; i < argc; i++){
-        strcat(c, argv[i]);
-    }
-    // checks for letters
+
+    // checks if string has anything other than 1 or 0
     char *p = c;
     for (i = 0; i < strlen(p); i++){
-        if(isalpha(p[i]) != 0){
-            printf("Error: only numbers are permitted.\n");
+        if(p[i] != '1' && p[i] != '0'){
+            printf("Only digits 1 and 0 are permitted.\n");
             return 0;
         }
     }
 
-    // convert numbers to ints
-    for(i = 1; i < (argc); i++)
-    {
-        numbers[i] = atoi(argv[i]);
-    }
-
-    count_numbers(numbers, argc);
-}
-
-
-// will print evens, odds, and primes or an error if there are negatives
-int count_numbers(int args[], int argc)
-{
-    int odds = 0, evens = 0, primes = 0;
-    int i, p;
-
-    for(i = 1; i < argc; i++)
-    {
-        p = check_prime(args[i]);
-
-        if(args[i] > 0){
-            // if prime
-            if(p == 1){
-                if(args[i] % 2 == 0){
-                    primes += 1;
-                    evens += 1;
-                }
-                else if(args[i] % 2 == 1){
-                    primes += 1;
-                    odds += 1;
-                }
-            }
-            // if not a prime
-            else if(p == 0){
-                if(args[i] % 2 == 0){
-                    evens += 1;
-                }
-                else if(args[i] % 2 == 1){
-                    odds += 1;
-                }
-            }
-        }
-        else{
-            printf("Error: Only positive integers are permitted.\n");
-            return 0;
-        }
-    }
-    printf("Number of odd numbers: %d\nNumber of even numbers: %d\nNumber of prime numbers: %d\n", odds, evens, primes);
+    int bin = atoi(c);
+    convert2bin(&bin);
+    printf("%d\n", result);
     return 0;
 }
 
 
-// returns 1 if prime, 0 otherwise
-int check_prime(int num)
+// convert intgers to binary
+void convert2bin(int *binary)
 {
-    int i, flag = 1;
-    
-    // checks if number is not a prime
-    for(i = 2; i <= num/2; i++){
-        if ((num % i) == 0){
-            flag = 0;
-            break;
-        }
+    int decimal = 0, i = 0, remainder, n;
+    n = *binary;
+    while (n != 0) {
+        remainder = n % 10;
+        n /= 10;
+        decimal += remainder * pow(2, i);
+        ++i;
     }
-
-    // resets flag to 0 if num is 1
-    // as 1 is not a prime number
-    if (num == 1){
-        flag = 0;
-    }
-    return flag;
+    result = decimal;
 }
