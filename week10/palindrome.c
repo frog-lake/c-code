419ae51f1104253381541d1f172867f4af8983ce
Frogger <66127998+frog-lake@users.noreply.github.com>
2020-12-07 18:35:47 +0000

    Update palindrome.c

--- week10/palindrome.c
+++ week10/palindrome.c
@@ -9,35 +9,31 @@
 int valid_flag = 0;
 // raised if input is a palindrome
 int palindrome_flag = 0;
-
-// so i dont have to delcare it all the time
+// so i isn't declared all the time
 int i;
-
-// cleaned string (no whitespace, punctuation... etc.)
-//char *cleaned = NULL;
-
+// the string that will be scrubbed and tested
 char *string = NULL;
 
+// function declarations
 void checker(char *ptr);
 void cleaner(char *ptr, char *dst);
 void if_palindrome(char *ptr);
-//void reformat_string(char *src, char *dst);
 
 
+// checks input for numbers and raises valid_flag if there are
 void checker(char *ptr)
 {
     for(i = 0; i < strlen(ptr); ++i)
     {
-        if(isdigit(ptr[i]) != 0){valid_flag = 1;break;}
+        if(isdigit(ptr[i]) != 0){valid_flag = 1; break;}
     }
 }
 
-void cleaner(char *src, char *dst) {
-    // cleans up trailing
-    if ((strlen(src) > 0) && (src[strlen (src) - 1] == '\n')){
-        src[strlen (src) - 1] = '\0';
-    }
 
+// scrubs punctuation and whitespace from the input
+void cleaner(char *src, char *dst) {
+
+    // copies alphabetic characters to dst and ignores everything else
     for (; *src; ++src){
         if (!ispunct((unsigned char) *src) && !isspace((unsigned char) *src)){
             *dst++ = tolower((unsigned char) *src);
@@ -47,15 +43,18 @@ void cleaner(char *src, char *dst) {
 }
 
 
+// raises palindrome_flag if entered string is a palindrome
 void if_palindrome(char *ptr)
 {
     int c = 0;
     int l = strlen(ptr);
+    // increments c
     for(i = 0; i < l; ++i){
         if(ptr[i] == ptr[l - i - 1]){
             ++c;
         }
     }
+    // if c is equal to i, then the string is a palindrome
     if(c==i){
         palindrome_flag = 1;
     }
@@ -64,21 +63,21 @@ void if_palindrome(char *ptr)
 
 int main()
 {
+    // get input
     char str[MAX_STRING_SIZE];
     printf("Please enter a string:\n");
     fgets(str, MAX_STRING_SIZE, stdin);
-    
     string = str;
-    
 
-    
     // check for digits
     checker(string);
     if(valid_flag == 1){printf("Invalid!!\n");return 0;}
     
+    // scrub and test input
     cleaner(string, string);
     if_palindrome(string);
     
+    // if flag was raised, then string is a palindrome, otherwise string is not a palindrome
     if(palindrome_flag == 0){
         printf("This string is not a palindrome.\n");
     }
